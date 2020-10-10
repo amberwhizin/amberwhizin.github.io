934a39fcbeaa4a637887f3ec29cb9f12cd11741b
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2020-10-10 14:11:05 -0400

    almost done switching to JQuery from hardcoding in HTML

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 $(() => {
-    
-})
+  const $columns = $("<div>").addClass("column");
+  $(".wrapper").append($columns);
+
+  const makeSquares = () => {
+    for (let i = 1; i < 4; i++) {
+      const $cells = $("<div>")
+        .addClass("cell")
+        .addClass("cell" + i)
+        .attr("size", "?") // come back
+      $columns.append($cells);
+    }
+  };
+  makeSquares();
+});
+
+// how do i make it do this 2 more times without making 2 more loops that do the same thing?

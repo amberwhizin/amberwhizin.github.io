6b2cfb4d34752224f476b9fd7c1de35860ed2e48
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2020-10-11 12:42:27 -0400

    created circle function and linking to column

--- js/app.js
+++ js/app.js
@@ -1,17 +1,33 @@
 $(() => {
-  const $columns = $("<div>").addClass("column");
-  $(".wrapper").append($columns);
+  //function that generates a circle when clicked
+  const generateCircle = () => {
+    const $circle = $("<div>").addClass("circle");
+    $(".wrapper").append($circle);
+    
+  };
 
-  const makeSquares = () => {
-    for (let i = 1; i < 4; i++) {
+  const makeSquares = ($column) => {
+    for (let i = 1; i < 6; i++) {
       const $cells = $("<div>")
         .addClass("cell")
         .addClass("cell" + i)
-        .attr("size", "?") // come back
-      $columns.append($cells);
+        .attr("size", "?"); // come back
+      $column.append($cells);
     }
   };
-  makeSquares();
-});
 
-// how do i make it do this 2 more times without making 2 more loops that do the same thing?
+  const generateColumn = (index) => {
+    const $column = $("<div>")
+      .addClass("column")
+      .addClass(`column${index}`)
+      .on("click", generateCircle);
+    makeSquares($column);
+    return $column;
+  };
+
+  const $column1 = generateColumn(1);
+  const $column2 = generateColumn(2);
+
+  $(".wrapper").append($column1);
+  $(".wrapper").append($column2);
+});

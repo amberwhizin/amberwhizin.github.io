4e06b5fd82c50f0c567a182e76fbc90e0b2c4723
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2020-10-11 15:47:08 -0400

    alomst worked out turn based game logic

--- js/app.js
+++ js/app.js
@@ -1,33 +1,61 @@
 $(() => {
   //function that generates a circle when clicked
+
+  let player1 = [];
+  let player2 = [];
+  // siting myself here via tic-tac-toe project
+  let isPlayer1 = true;
+  const getPlayersTurn = () => {
+    const turn = isPlayer1
+      ? (player1 += generateCircle())
+      : (player2 += console.log("yellow"));
+    isPlayer1 = !isPlayer1;
+    return turn;
+  };
+
   const generateCircle = () => {
     const $circle = $("<div>").addClass("circle");
     $(".wrapper").append($circle);
-    
+    // if (player1 !== []) {
+    //     $('.circle').attr('background', 'yellow')
+    // } else {
+        
+    // }
+
   };
 
   const makeSquares = ($column) => {
-    for (let i = 1; i < 6; i++) {
+    for (let i = 1; i <= 7; i++) {
       const $cells = $("<div>")
         .addClass("cell")
         .addClass("cell" + i)
-        .attr("size", "?"); // come back
+        .on("click", getPlayersTurn);
       $column.append($cells);
     }
   };
 
   const generateColumn = (index) => {
-    const $column = $("<div>")
-      .addClass("column")
-      .addClass(`column${index}`)
-      .on("click", generateCircle);
+    const $column = $("<div>").addClass("column").addClass(`column${index}`);
     makeSquares($column);
     return $column;
   };
-
+  // how do i get the circle in the column?
   const $column1 = generateColumn(1);
   const $column2 = generateColumn(2);
+  const $column3 = generateColumn(3);
+  const $column4 = generateColumn(4);
+  const $column5 = generateColumn(5);
+  const $column6 = generateColumn(6);
 
   $(".wrapper").append($column1);
   $(".wrapper").append($column2);
+  $(".wrapper").append($column3);
+  $(".wrapper").append($column4);
+  $(".wrapper").append($column5);
+  $(".wrapper").append($column6);
+
+  // game logic
+
+  // some where that holds who turn it is
+  // turns will be based on clicks, when a player clicks on a square then the index of that square is pushed into an array to keep track of ?
 });

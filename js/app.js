473afbe36e53bb2e8fc3c0b132d2b9a9df36baa5
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2020-10-12 17:27:50 -0400

    tracking cells that circles are in

--- js/app.js
+++ js/app.js
@@ -1,19 +1,24 @@
 $(() => {
-  //function that generates a circle when clicked
+  const yellow = "yellow";
+  const red = "red";
 
-  const column0 = ["RED", "YELLOW"];
-  const column1 = ["RED"];
-  const column2 = ["YELLOW"];
-
-  let colors = [];
-  let player2 = [];
+  const column0 = [];
+  const column1 = [];
+  const column2 = [];
+  const column2 = [];
+  const column2 = [];
+  const column2 = [];
+  const column6 = [];
+  const board = [column0, column1, column2, [], [], [], column6];
+  debugger;
+  // if not player1 turn then player2
   // siting myself here via tic-tac-toe project
   let isPlayer1 = true;
   const getIsPlayer1Turn = () => {
     isPlayer1 = !isPlayer1;
     return isPlayer1;
   };
-
+  // returns red circles, if true is passed in then return yellow
   const generateCircle = (isYellow = false) => {
     const $circle = $("<div>").addClass("circle");
     if (isYellow) {
@@ -22,32 +27,54 @@ $(() => {
     return $circle;
   };
 
-  const makeCells = ($column) => {
+  // invokes circles when screen clicked, switches between the two
+  const makeCells = ($column, columnInfo) => {
     for (let i = 0; i < 6; i++) {
+      const piece = columnInfo[i];
       const $cell = $("<div>")
         .addClass("cell")
-        .addClass("cell" + i)
-        .on("click", () => {
-          const isPlayer1Turn = getIsPlayer1Turn();
-          const $circle = generateCircle(isPlayer1Turn);
-          $cell.append($circle).off("click");
-        });
+        .addClass("cell" + i);
+      // .on("click", () => {
+      //   const isPlayer1Turn = getIsPlayer1Turn();
+      //   if (isPlayer1Turn === column0) {
+      //      column0 += piece;
+      //   }
+      //   const $circle = generateCircle(isPlayer1Turn);
+      //   $cell.append($circle).off("click");
+      // });
+      if (piece === yellow) {
+        const $circle = generateCircle(true);
+        $cell.append($circle);
+      }
+      if (piece === red) {
+        const $circle = generateCircle();
+        $cell.append($circle);
+      }
       $column.append($cell);
     }
   };
-
+  // creates columns with cells in them
   const generateColumn = (index) => {
     const $column = $("<div>").addClass("column").addClass(`column${index}`);
-    makeCells($column);
+    const currentColumnInfo = board[index];
+    console.log({ currentColumnInfo });
+    makeCells($column, currentColumnInfo); // holds info for my cells
     return $column;
   };
+  // makes multiple columns
+  const render = () => {
+    for (let i = 0; i < 7; i++) {
+      debugger;
+      const $column = generateColumn(i);
+      $(".wrapper").append($column);
+    }
+  };
 
-  for (let i = 0; i < 7; i++) {
-    const $column = generateColumn(i);
-    $(".wrapper").append($column);
-  }
+  // cell5 is clicked
+  //cell5.shift
+  // cell5.unshift
 
-  
-  // some where that holds who turn it is
-  // turns will be based on clicks, when a player clicks on a square then the index of that square is pushed into an array to keep track of ?
+  //if cell is clicked, and its red, push it into the array, then render the array position
+  //you click in column 0,
+  //go to the earliest position
 });
